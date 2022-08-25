069f04b944f505849e81fa28ca5f4b1c375d427e
rohit-chendur <rohit.s.chendur@gmail.com>
2022-08-25 11:40:01 +0530

    1

--- src/main/java/com/sonata/daythree/DemoApplication.java
+++ src/main/java/com/sonata/daythree/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	
-	public int add(int a,int b) {
-		return a+b;
+	public int mul(int a,int b) {
+		return a*b;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/test/java/com/sonata/daythree/DemoApplicationTests.java
+++ src/test/java/com/sonata/daythree/DemoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.sonata.daythree;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoApplicationTests {
 	DemoApplication d1 = new DemoApplication();
-	@Test
-	public void addtest() {
-		assertEquals(500,d1.add(200, 300));
+	//@Test
+	public void multest() {
+		assertEquals(500,d1.mul(250, 2));
 	}
 
 }
