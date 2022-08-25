de4e0dee94d3fadfd835383e7e67044db23c3c8f
rohit-chendur <rohit.s.chendur@gmail.com>
2022-08-25 11:44:06 +0530

    1

--- src/test/java/com/sonata/daythree/DemoApplicationTests.java
+++ src/test/java/com/sonata/daythree/DemoApplicationTests.java
@@ -1,11 +1,12 @@
 package com.sonata.daythree;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoApplicationTests {
 	DemoApplication d1 = new DemoApplication();
-	//@Test
+	
+	@Test
 	public void multest() {
 		assertEquals(500,d1.mul(250, 2));
 	}
